c338f4e5e02c90a2d94f5fbd4d55b42d6511ff78
Shazil Sattar <sattarshazil@gmail.com>
2021-04-27 01:08:35 +0500

    Updates

---
+++ precache-manifest.6c318c1876ea89a795568fd387b6a464.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ffcc31caa3bfe9c018913755530e4b",
+    "url": "/todolist-redux/index.html"
+  },
+  {
+    "revision": "2fb183878c62591386b9",
+    "url": "/todolist-redux/static/js/2.2bddec5e.chunk.js"
+  },
+  {
+    "revision": "597dedda2f44d26475c3",
+    "url": "/todolist-redux/static/js/main.b5ce6afc.chunk.js"
+  },
+  {
+    "revision": "e1760aa4fbd6d436e927",
+    "url": "/todolist-redux/static/js/runtime-main.6bb6f532.js"
+  }
+]);
